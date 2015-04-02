2f960f6307f3650261e2108f8c5f5a00b72b3f7c
Gio d'Amelio <giodamelio@gmail.com>
2015-04-02 03:28:13 -0700

    Make sure we get an entry file and check it exists

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+var assert = require("assert");
+var fs = require("fs");
+var path = require("path");
+
 var rc = require("rc");
 
 // Load config
 var config = rc("adventurer", {});
+
+try {
+    // Check if entry is given
+    assert.equal(config._.length, 1, "You must provide ONE entry file");
+
+    // Check that entry exists
+    assert.ok(fs.existsSync(path.join(__dirname, config._[0])), "Entry file must exist");
+} catch (e) {
+    console.log(e.message);
+    process.exit(1);
+}
 
 console.log(config);
 
