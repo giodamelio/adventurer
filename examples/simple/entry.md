17c2d0fd05522e20f60fc6d01bad593174f69efa
Gio d'Amelio <giodamelio@gmail.com>
2015-04-02 03:50:54 -0700

    Parse front-matter with grey-matter

---
+++ examples/simple/entry.md
@@ -0,0 +1,11 @@
+---
+type: "entry"
+links:
+    - text: "The End" 
+      filename: "the_end.md"
+    - text: "A new beginning"
+      filename: "a_new_beginning.md"
+---
+
+Hello World
+
--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 var assert = require("assert");
 var fs = require("fs");
 var path = require("path");
+var util = require("util");
 
 var rc = require("rc");
 var colors = require("colors");
+var grayMatter = require("gray-matter");
 
 // Load config
 var config = rc("adventurer", {});
 
+var entryFile;
 try {
     // Check if entry is given
     assert.equal(config._.length, 1, "You must provide ONE entry file");
 
+    // Full path to entry file
+    entryFile = path.join(process.cwd(), config._[0]);
+
     // Check that entry exists
-    assert.ok(fs.existsSync(path.join(__dirname, config._[0])), "Entry file must exist");
+    assert.ok(fs.existsSync(entryFile), "Entry file must exist");
 } catch (e) {
     console.log(e.message.red);
     process.exit(1);
 }
 
-console.log(config);
+// Parse entry
+var entry = fs.readFileSync(entryFile);
+entry = grayMatter(entry.toString());
+console.log(util.inspect(entry));
 
